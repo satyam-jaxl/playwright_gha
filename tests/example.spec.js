794a9cac7878d73414537b3de5dd8f386e198cf5
satyam-jaxl <satyam.seth@jaxl.com>
2022-04-21 10:36:19 +0530

    1st commit

---
+++ tests/example.spec.js
@@ -0,0 +1,21 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Test Home Page', () => {
+  test('Home page must contain text hello world', async ({ page }) => {
+    await page.goto('/')
+    
+    const text = await page.locator('body').innerText();
+    
+    await expect(text).toEqual('Hello World!');
+
+  });
+
+  test('Failed', async ({ page }) => {
+    await page.goto('/')
+    
+    const text = await page.locator('body').innerText();
+    
+    await expect(text).toEqual('Oops!');
+
+  });
+});
